5ead0a6333127c4be3f66b48a4a35fd7b8fa566a
David Wenzel <dwenzel95@gmail.com>
2023-01-21 16:43:20 -0600

    sections controller file added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_21_221457) do
+ActiveRecord::Schema.define(version: 2023_01_21_222108) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string "goal"
+    t.integer "section_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sections", force: :cascade do |t|
     t.string "title"
