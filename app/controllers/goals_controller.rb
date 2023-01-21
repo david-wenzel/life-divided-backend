c278dce929c6e763a04c451dfd4e013c36472823
David Wenzel <dwenzel95@gmail.com>
2023-01-21 17:53:44 -0600

    delete goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -11,13 +11,20 @@ class GoalsController < ApplicationController
     )
     goal.to_json
     end
-    
+
 # update a goal
     patch "/goals/:id" do
         goal = Goal.find_by(id: params[:id])
         goal.update(
             goal: params[:goal]
         )
+        goal.to_json
+    end
+
+    # delete goal
+    delete "/goals/:id" do
+        goal = Goal.find_by(id: params[:id])
+        goal.destroy
         goal.to_json
     end
 
