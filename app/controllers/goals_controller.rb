8efaf675da736d2d321ef641286d630d0ebb3f45
David Wenzel <dwenzel95@gmail.com>
2023-01-21 16:52:16 -0600

    goals post route added to add new goal and accept params

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,3 +1,10 @@
 class GoalsController < ApplicationController
- 
+ # Add a new goal
+ post "/goals" do 
+    event = Goal.create(
+        goal: params[:goal],
+        section_id: params[:section_id]
+    )
+    event.to_json
+end
 end
