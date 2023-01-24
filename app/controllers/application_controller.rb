7d9a127d3e5510c41ac697eacde17b45a707ef6d
David Wenzel <dwenzel95@gmail.com>
2023-01-23 19:39:26 -0600

    rename all lists to all sections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-def all_lists
+def all_sections
   Section.all.order(updated_at: :desc).to_json(include: :goals)
 end
 
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,28 +1,15 @@
 class SectionsController < ApplicationController
 
-# this controller hanldes sections routes
+# this controller hanldes sections
 
-   # Get all sections alphabetically by name
-    # return a JSON response with an array of sections data, including nested goals data for each section
     get "/sections" do
-        # section = Section.all.order(:updated_at)
-        # Section.all.order(updated_at: :desc).to_json(include: :goals)
-        all_lists
+        all_sections
     end
-
-    # post "/sections" do
-    #     section = Section.create(
-    #         title: params[:title]
-    #     )
-    #     section.to_json
-
-    # end
 
     # backend opperation only 
     delete "/sections/:id" do
-        section = Section.find_by(id: params[:id])
-        section.destroy
-        section.to_json
+        Section.find_by(id: params[:id])
+        all_lists
     end
 
 
@@ -31,7 +18,7 @@ class SectionsController < ApplicationController
             title: params[:title]
         )
         # Section.all.order(updated_at: :desc).to_json(include: :goals)
-        all_lists
+        all_sections
     end
 
 end
